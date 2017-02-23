b0b4bcb1635194a6acbcf8287fe065701cf17494
Andres <asalaspena@gmail.com>
2017-02-23 17:23:47 +0100

    1 pipe 1 scanf

--- Practica 1/Ejercicio9/ejercicio9.c
+++ Practica 1/Ejercicio9/ejercicio9.c
@@ -1,31 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 
 #define NUM_PROC 4
-int main (void){
-    int fpid = 1;
-    int pid;
-    int ppid;
+int main (int argc,char* argv[]){
     int i;
+    int fd[2],nbytes,pipe_status;
+    pid_t childpid;
+    char readbuffer[150];
+    int op1,op2;
+    char string[100];
+
+    printf("Introduce el primer numero: ");
+    scanf("%d", &op1);
+    printf("Introduce el segundo numero: ");
+    scanf("%d", &op2);
+
     for (i=0; i < NUM_PROC; i++){
-        if (fpid == 0){
-            pid = getpid();
-            ppid = getppid();
-            printf("HIJO %d: PID PADRE: %d\t PID HIJO: %d\n", i, ppid, pid);
-        }else{
-            if ((fpid=fork()) < 0){
-                printf("Error al emplear fork\n");
-                exit(EXIT_FAILURE);
-            }
-            pid = getpid();
-            printf ("PADRE %d: PID %d \n", i, pid);
+        pipe_status = pipe(fd);
+        if(pipe_status == -1){
+	    perror("Error creando la tuberia\n");
+            exit(EXIT_FAILURE);
         }
-    }
-    wait(NULL);
-    getchar();
-    exit(EXIT_SUCCESS);
-} 
+        if ((childpid = fork()) == -1){
+            perror("Error al emplear fork\n");
+            exit(EXIT_FAILURE);
+        }
+        if (childpid == 0){
+		nbytes = read(fd[0],readbuffer,sizeof(readbuffer));
+		sscanf(readbuffer,"%d,%d",&op1,&op2);
+		if(i==0){
+			printf("Datos enviados a través de la tubería por el proceso PID=%d. ",getpid());
+			printf("Operando 1: %d. ",op1);
+			printf("Operando 2: %d. ",op2);
+			printf("Suma: %d.\n",op1+op2);
+        }
+		else if(i==1){
+			printf("Datos enviados a través de la tubería por el proceso PID=%d. ",getpid());
+			printf("Operando 1: %d. ",op1);
+			printf("Operando 2: %d. ",op2);
+			printf("Resta: %d.\n",op1-op2);
+        }
+		else if(i==2){
+			printf("Datos enviados a través de la tubería por el proceso PID=%d. ",getpid());
+			printf("Operando 1: %d. ",op1);
+			printf("Operando 2: %d. ",op2);
+			printf("Multiplicacion: %d.\n",op1*op2);
+        }
+		else{
+			printf("Datos enviados a través de la tubería por el proceso PID=%d. ",getpid());
+			printf("Operando 1: %d. ",op1);
+			printf("Operando 2: %d. ",op2);
+			printf("Division: %d.\n",op1/op2);
+        }
+	exit(EXIT_SUCCESS);
+	}
+        else{
+	    sprintf(string,"%d,%d",op1,op2);
+	    write(fd[1],string,strlen(string));
+	    wait(NULL);
+        }
+    }    exit(EXIT_SUCCESS);
+}
