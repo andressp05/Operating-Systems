ea8f33687ab4541344054ab5a437cd1fe3a1a5b4
homerjr <antoniommasuda@gmail.com>
2017-02-26 10:56:11 +0100

    Ejercicio 8 terminado

--- Practica 1/Ejercicio8/ejercicio8.c
+++ Practica 1/Ejercicio8/ejercicio8.c
@@ -5,37 +5,78 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-void main(int argc, char* argv[]){
-	int i;
-	int fpid = 1;
-    int pid;
-    int ppid;
-    char *aux[2];
+char* getPrograma(char* path){
+    char* prog;
+    char* ret;
+    char ch = '/';
+    
+    ret = strrchr(path, ch);
+    if(ret == NULL)
+        return NULL;
+    
+    prog = strchr(ret, ret[1]);
+    
+    return prog;
+}
 
-	for(i = 0; i < argc; i++){
+int main(int argc, char* argv[]){
+	int i;
+	int fpid;
+	
+	if(argc < 3){
+	    printf("El número de argumentos introducidos no es suficiente\n");
+	    exit(EXIT_FAILURE);
+	}
+
+	for(i = 1; i < argc - 1; i++){
+	    if ((fpid = fork()) < 0){
+            printf("Error al emplear fork\n");
+            exit(EXIT_FAILURE);
+        }
+        wait(NULL);
+        
 		if (fpid == 0){
             if(strcmp("-l", argv[argc-1]) == 0){
-            	execl(argv[i],argv[i], NULL);
+                char* prog;
+                prog = getPrograma(argv[i]);
+                if(prog == NULL){
+                    printf("Se debe introducir la ruta del programa\n");
+                    return EXIT_FAILURE;
+                }
+            	execl(argv[i], prog, NULL);
+            	printf("Programa %d no ha funcionado\n", i);
+            	return EXIT_FAILURE;
             }
             else if(strcmp("-lp", argv[argc-1]) == 0){
             	execlp(argv[i], argv[i], NULL);
+            	printf("Programa %d no ha funcionado\n", i);
+            	return EXIT_FAILURE;
             }
             else if(strcmp("-v", argv[argc-1]) == 0){
-            	aux[0]=argv[i];
-            	aux[1]=NULL;
-            	execv(argv[i],aux);
+                char* aux[2];
+                aux[0] = getPrograma(argv[i]);
+                if(aux[0] == NULL){
+                    printf("Se debe introducir la ruta del programa\n");
+                    return EXIT_FAILURE;
+                }
+                aux[1] = NULL;
+            	execv(argv[i], aux);
+            	printf("Programa %d no ha funcionado\n", i);
+            	return EXIT_FAILURE;
             }
             else if(strcmp("-vp", argv[argc-1]) == 0){
-            	aux[0]=argv[i];
-            	aux[1]=NULL;
+                char *aux[2];
+            	aux[0] = argv[i];
+            	aux[1] = NULL;
             	execvp(argv[i], aux);
+            	printf("Programa %d no ha funcionado\n", i);
+            	return EXIT_FAILURE;
             }
-        }else{
-            if ((fpid=fork()) < 0){
-                printf("Error al emplear fork\n");
+            else{
+                printf("El último parámetro no es válido\n");
                 exit(EXIT_FAILURE);
             }
-            
         }
 	}
+	exit(EXIT_SUCCESS);
 }
