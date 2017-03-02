b6c6c08a3e1203b0412ef4f24ef04808e0036479
homerjr <antoniommasuda@gmail.com>
2017-03-02 22:04:35 +0100

    Código definitivo

--- Practica 1/Ejercicio4/ejercicio4ai.c
+++ Practica 1/Ejercicio4/ejercicio4ai.c
@@ -24,17 +24,17 @@ int main (void){
    
    int pid;
    int i;
+   
    for (i=0; i < NUM_PROC; i++){
       if ((pid = fork()) < 0 ){
          printf("Error al emplear fork\n");
          exit(EXIT_FAILURE);
-      }
-       else if (pid == 0){
+      } else if (pid == 0){
          printf("HIJO %d\n", i);
-       }else{
+      } else{
          printf ("PADRE %d \n", i);
-       }
+      }
    }
-exit(EXIT_SUCCESS);
+   exit(EXIT_SUCCESS);
 
 }
